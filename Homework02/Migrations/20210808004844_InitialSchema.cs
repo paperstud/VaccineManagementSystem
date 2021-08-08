489def63177d9bb4ead863ff1aaedcc0ba72514b
paperstud <stevegalvan@LIVE.COM>
2021-08-07 17:49:53 -0700

    f

---
+++ Homework02/Migrations/20210808004844_InitialSchema.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Homework02.Migrations
+{
+    public partial class InitialSchema : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Vaccines",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TotalDoses = table.Column<int>(type: "int", nullable: false),
+                    DosesRequired = table.Column<int>(type: "int", nullable: false),
+                    DaysBetween = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vaccines", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Vaccines");
+        }
+    }
+}
---
+++ Homework02/Models/Patient.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Homework02.Models
+{
+    public class Patient
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime FirstDose { get; set; }
+        public DateTime SecondDose { get; set; }
+        public virtual Vaccine Vaccine { get; set; }
+
+        public override string ToString()
+        {
+            return $"[{Id}, {Name}, {FirstDose:d} , {SecondDose:d}]";
+        }
+    }
+}
